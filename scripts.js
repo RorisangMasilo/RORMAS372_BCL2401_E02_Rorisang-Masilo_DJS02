942b214273525e17083631ce9be681ad88b82cf1
Rori <rbmasilo@gmail.com>
2024-05-11 10:40:39 +0200

    added validation for division by zero

--- scripts.js
+++ scripts.js
@@ -14,6 +14,14 @@ form.addEventListener("submit", (event) => {
     return;
   }
 
+  // Add validation for division by zero
+  if (parseInt(divider) === 0) {
+    result.innerText =
+      "Division not performed. Invalid number provided. Try again";
+    console.error("Division by zero error: ", new Error().stack);
+    return;
+  }
+
   if (dividend === "" || divider === "") {
     result.innerText =
       "Division not performed. Both values are required in inputs. Try again";
