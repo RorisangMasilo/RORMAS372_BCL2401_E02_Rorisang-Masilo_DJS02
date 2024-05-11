af999fc893e181b0f15c030ed6fd6f8bb1e04188
Rori <rbmasilo@gmail.com>
2024-05-11 10:38:34 +0200

    implement validation method using if loops

--- scripts.js
+++ scripts.js
@@ -3,7 +3,21 @@ const result = document.querySelector("[data-result]");
 
 form.addEventListener("submit", (event) => {
   event.preventDefault();
+
   const entries = new FormData(event.target);
   const { dividend, divider } = Object.fromEntries(entries);
-  result.innerText = dividend / divider;
+
+  // Add validation for empty inputs
+  if (!dividend || !divider) {
+    result.innerText =
+      "Division not performed. Both values are required in inputs. Try again";
+    return;
+  }
+
+  if (dividend === "" || divider === "") {
+    result.innerText =
+      "Division not performed. Both values are required in inputs. Try again";
+  } else {
+    result.innerText = dividend / divider;
+  }
 });
